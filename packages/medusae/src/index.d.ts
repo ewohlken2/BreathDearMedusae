9ca10580a2bb10f9ae5551cf2b15c073083c43c5
Elvis Wohlken <ewohlken@gmail.com>
2026-02-04 17:47:30 -0800

    Add medusae package updates

---
+++ packages/medusae/src/index.d.ts
@@ -0,0 +1,33 @@
+import type { CSSProperties } from "react";
+
+export interface MedusaeConfig {
+  cursor?: {
+    radius?: number;
+    strength?: number;
+    dragFactor?: number;
+  };
+  halo?: {
+    outerOscFrequency?: number;
+    outerOscAmplitude?: number;
+    radiusBase?: number;
+    radiusAmplitude?: number;
+    shapeAmplitude?: number;
+    rimWidth?: number;
+    outerStartOffset?: number;
+    outerEndOffset?: number;
+  };
+  particles?: {
+    baseSize?: number;
+    activeSize?: number;
+    blobScaleX?: number;
+    blobScaleY?: number;
+  };
+}
+
+export interface MedusaeProps {
+  className?: string;
+  style?: CSSProperties;
+  config?: MedusaeConfig;
+}
+
+export declare function Medusae(props: MedusaeProps): JSX.Element;
